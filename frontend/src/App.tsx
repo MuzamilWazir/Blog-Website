b91920b14a6e992e6e4b8fd188749cb9fe3c7397
MuzamilWazir <wazirmuhammadmuzammil@gmail.com>
2026-07-01 15:54:17 +0500

    feat: implement routing and add authentication components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import LoginForm from "./components/Auth/LoginForm";
+import SignupForm from "./components/Auth/SignupForm";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
 
-export default App
+        <Route path="/" index element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
