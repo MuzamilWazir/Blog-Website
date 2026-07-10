97b9bb6448a4f2cf7ae4bcf252c02302f8b34716
MuzamilWazir <wazirmuhammadmuzammil@gmail.com>
2026-07-10 13:30:43 +0500

    feat: integrate Tailwind CSS and update Navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,12 +2,12 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = (): React.JSX.Element => {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
+    <nav className="flex justify-center space-x-10 p-5 border-b border-black shadow-b-black font-bold text-md">
+      <NavLink className="" to="/">Home</NavLink>
       <NavLink to="/blogs">Blogs</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/login">Login</NavLink>
-      <NavLink to="/signup">Signup</NavLink>
+      <NavLink to="/signup">Signup</NavLink> 
       <NavLink to="/profile">Profile</NavLink>
     </nav>
   );
